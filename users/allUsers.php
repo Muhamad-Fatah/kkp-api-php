cf4c975663a6a12adac14f9e650cc4409443b103
Muhamad-Fatah <muhamad.fatah494@gmail.com>
2023-07-14 21:57:14 +0700

    fix: fix listAllUser endpoint

--- users/allUsers.php
+++ users/allUsers.php
@@ -58,4 +58,32 @@ if ($user_id) {
     }
 }
 
+$sql = "SELECT user_id, nama_depan, nama_belakang, role, jurusan, kegiatan_id FROM tbl_user";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    $response = array(
+        "success" => true,
+        "message" => "Success get all users",
+        "data" => array()
+    );
+
+    while ($row = mysqli_fetch_assoc($result)) {
+        $response["data"][] = $row;
+    }
+
+    $json = json_encode($response);
+
+    echo $json;
+} else {
+    $response = array(
+        "success" => false,
+        "message" => "No list user showing",
+    );
+
+    $json = json_encode($response);
+
+    echo $json;
+}
+
 mysqli_close($conn);
