b21a39d8304abdcb30587e78144ed1f655370000
Muhamad-Fatah <muhamad.fatah494@gmail.com>
2023-07-14 20:15:32 +0700

    feature: add endpoint get users by id

--- users/allUsers.php
+++ users/allUsers.php
@@ -3,6 +3,7 @@
 require_once "../connection.php";
 
 $role = $_GET["role"] ?? "";
+$user_id = $_GET["user_id"] ?? "";
 
 if ($role) {
     $sql = "SELECT user_id, nama_depan, nama_belakang, role, jurusan, kegiatan_id FROM tbl_user WHERE role = '$role'";
@@ -23,20 +24,33 @@ if ($role) {
 
         echo $json;
     }
-} else {
-    $sql = "SELECT user_id, nama_depan, nama_belakang, role, jurusan, kegiatan_id FROM tbl_user";
+
+    return;
+}
+
+if ($user_id) {
+    $sql = "SELECT user_id, nama_depan, nama_belakang, role, jurusan, kegiatan_id FROM tbl_user WHERE user_id = '$user_id'";
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($result) > 0) {
         $response = array(
             "success" => true,
-            "message" => "Success get list all users",
-            "data" => array()
+            "message" => "Success get user by id $user_id",
+            "data" => $result
         );
 
         while ($row = mysqli_fetch_assoc($result)) {
-            $response["data"][] = $row;
+            $response["data"] = $row;
         }
+
+        $json = json_encode($response);
+
+        echo $json;
+    } else {
+        $response = array(
+            "success" => false,
+            "message" => "UserId not found",
+        );
 
         $json = json_encode($response);
 
